916790e0bfa843e876aa43c5752467172c637bb0
Emre Dağ <ewredag@gmail.com>
2022-05-30 00:02:33 +0300

    Created REST API for project

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.status(200).json([
+        {
+            job_name: 'Urgent',
+            job_id: "1",
+        },
+        {
+            job_name: 'Regular',
+            job_id: "2",
+        },
+        {
+            job_name: 'Trivial',
+            job_id: "3",
+        }
+    ])
+})
+
+module.exports = app;
