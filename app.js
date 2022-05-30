a16904812353355295d7351006bba87692d2cc34
Emre Dağ <ewredag@gmail.com>
2022-05-30 07:36:13 +0300

    Cors added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('express');
 
+const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
 
 app.use((req, res, next) => {
     res.status(200).json([
